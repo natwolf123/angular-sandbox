33ae97de02165237d3a48ddf3b61dc2bd68d42ff
Natalie Wolf <Natalie.Wolf@chrobinson.com>
2017-02-28 16:11:45 -0600

    look i made an angular app that can be hosted in github pages

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularSandboxApp
+ * @description
+ * # angularSandboxApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularSandboxApp', []);
